5848900a125b3abab7f11b23cc0870ba588aeee2
codebude <codebude@users.noreply.github.com>
2026-09-15 06:21:26 +0000

    deploy: 733e9df06bceed2023a801d25bd9b003fb9d077d

---
+++ next/assets/chunks/swimlanesDiagram-VK2B7HYN.B6VZ_GKG.js
@@ -0,0 +1,8 @@
+import{c as r,s as e}from"./flowDiagram-A5DVABFB.3RBNYmmr.js";import{d as a}from"../app.CMKJhuhe.js";import"./chunk-5VM5RSS4.C3Yjf5jI.js";import"./chunk-XXDRQBXY.Bz020ukD.js";import"./chunk-POPQ4Y6H.DEkzVHb3.js";import"./chunk-F27PBJKO.CFkE6eFD.js";import"./channel.B8I1pRJA.js";import"./framework.BvWD_EvC.js";import"./theme.bC-05ogm.js";var o=a(t=>`${e(t)}
+  .swimlane.cluster rect {
+    stroke: ${t.clusterBorder} !important;
+  }
+  [data-look="neo"].cluster rect {
+    filter: none;
+  }
+`,"getStyles"),m=o,y=r({defaultLayout:"swimlane",styles:m});export{y as diagram};
